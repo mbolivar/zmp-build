b5a1146c5f910d1076c223dd5d42c2b537125e68
Marti Bolivar <marti@opensourcefoundries.com>
2018-02-14 13:59:57 -0500

    prep-release-notes.py: use pinned manifests as source data
    The current behavior of starting from a baseline commit and going
    until each repository's HEAD is not correct. Update builds are
    constructed from zmp-manifest builds which receive E2E treatment, and
    may be older than the current HEADs.
    
    To make this work more conveniently, ask for pinned manifests for the
    previous (start) and current (end) update, and prep release notes
    based on that.
    
    Signed-off-by: Marti Bolivar <marti@opensourcefoundries.com>

--- prep-release-notes.py
+++ prep-release-notes.py
@@ -7,6 +7,7 @@ from itertools import dropwhile, takewhile
 import os
 from os.path import abspath, join
 import textwrap
+from xml.etree import ElementTree
 
 import click
 import pygit2
@@ -28,11 +29,13 @@ def is_mergeup_commit(commit):
     return any(tag in short for tag in MERGEUP_SAUCE_TAGS)
 
 
-def mergeup_commits(repository_path, baseline_commit):
+def mergeup_commits(repository_path, start_sha, end_sha):
     repository = pygit2.init_repository(repository_path)
     sort = pygit2.GIT_SORT_TIME | pygit2.GIT_SORT_TOPOLOGICAL
-    walker = repository.walk(repository.head.target, sort)
-    walker.hide(baseline_commit)
+    start = repository.revparse_single(start_sha).oid
+    end = repository.revparse_single(end_sha).oid
+    walker = repository.walk(end, sort)
+    walker.hide(start)
     return [c for c in walker if is_mergeup_commit(c)]
 
 
@@ -56,10 +59,10 @@ def commit_date(commit):
     return datetime.fromtimestamp(author_timestamp, author_tz)
 
 
-def repo_mergeup_highlights(repo, baseline, yaml_indent):
+def repo_mergeup_highlights(repo, start, end, yaml_indent):
     out = StringIO()
 
-    mergeups = mergeup_commits(repo, baseline)
+    mergeups = mergeup_commits(repo, start, end)
     highlights = collections.OrderedDict((m, mergeup_highlights(m))
                                          for m in mergeups)
     missing = []
@@ -99,25 +102,32 @@ def repo_mergeup_highlights(repo, baseline, yaml_indent):
     return out.getvalue()
 
 
+def project_revisions(pinned_manifest):
+    return {elt.attrib['name']: elt.attrib['revision']
+            for elt in ElementTree.parse(pinned_manifest).getroot()
+            if elt.tag == 'project'}
+
+
 @click.command()
-@click.option('-z', '--zephyr-baseline', required=True,
-              help='Zephyr baseline commit')
-@click.option('-m', '--mcuboot-baseline', required=True,
-              help='MCUboot baseline commit')
 @click.option('--zmp', help='ZMP installation directory, default is cwd')
 @click.option('--yaml-indent', type=int, default=DEFAULT_INDENT,
               help='YAML indentation')
-def main(zephyr_baseline, mcuboot_baseline, zmp, yaml_indent):
+@click.argument('start-manifest')
+@click.argument('end-manifest')
+def main(start_manifest, end_manifest, zmp, yaml_indent):
     if zmp is None:
         zmp = abspath(os.getcwd())
     zmp = abspath(zmp)
     zephyr = join(zmp, 'zephyr')
     mcuboot = join(zmp, 'mcuboot')
 
-    zephyr_highlights = repo_mergeup_highlights(zephyr, zephyr_baseline,
-                                                yaml_indent)
-    mcuboot_highlights = repo_mergeup_highlights(mcuboot, mcuboot_baseline,
-                                                 yaml_indent)
+    start = project_revisions(start_manifest)
+    end = project_revisions(end_manifest)
+
+    zephyr_highlights = repo_mergeup_highlights(zephyr, start['zephyr'],
+                                                end['zephyr'], yaml_indent)
+    mcuboot_highlights = repo_mergeup_highlights(mcuboot,  start['mcuboot'],
+                                                 end['mcuboot'], yaml_indent)
 
     print('#', '=' * 70)
     print('# Zephyr highlights:')
