465d9dd2327fc1d89c11d9bf8b04e9192541cff2
Marti Bolivar <marti@foundries.io>
2018-10-01 18:09:00 -0600

    Move core.py into commands.py
    It's high time we start consolidating the zmp code. We've long since
    abandoned adding new features to it in favor of just wrapping west
    with backwards compatible options.
    
    Signed-off-by: Marti Bolivar <marti@foundries.io>

--- commands.py
+++ commands.py
@@ -5,20 +5,31 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import abc
+import glob
 import importlib
 import multiprocessing
 import os
+import platform
 import re
 import shlex
+import shutil
 import subprocess
 import sys
 
-from core import find_default_outdir, find_zephyr_base, \
-                 find_arm_none_eabi_gcc, \
-                 find_mcuboot_root, find_mcuboot_outdir, \
-                 find_app_root, find_app_outdir, check_boards, \
-                 check_dependencies, find_sdk_build_root
+# We could be smarter about this (search for .repo, e.g.), but it seems
+# unnecessary.
+ZMP_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
+# Checked out paths for important repositories relative to ZMP root.
+# TODO: parse these from the repo manifest, for robustness, at some point.
+ZEPHYR_PATH = 'zephyr'
+
+# The name of the directory which is the default root of the build hierarchy,
+# relative to the .repo root.
+BUILD_DIR_DEFAULT = 'outdir'
+
+# Where mcuboot is relative to the .repo top level.
+MCUBOOT_PATH = 'mcuboot'
 
 # Default values shared by multiple commands.
 BOARD_DEFAULT = 'nrf52_blenano2'
@@ -45,6 +56,116 @@ MCUBOOT_IMGTOOL = os.path.join('scripts', 'imgtool.py')
 # Any globally desirable CMake options can be added here.
 CMAKE_OPTIONS = []
 
+
+#
+# Helpers
+#
+
+def find_zmp_root():
+    '''Get absolute path of root directory of this ZMP installation.'''
+    return ZMP_ROOT
+
+
+def find_zephyr_base():
+    '''Get absolute path of ZMP Zephyr base directory.'''
+    return os.path.join(find_zmp_root(), ZEPHYR_PATH)
+
+
+def find_arm_none_eabi_gcc():
+    '''Get absolute path of ZMP prebuilt GCC ARM Embedded.'''
+    platform_subdirectories = {
+        'Linux': 'linux',
+        'Darwin': 'mac',
+        }
+    subdir = platform_subdirectories[platform.system()]
+    return os.path.join(find_zmp_root(),
+                        'build',
+                        'other',
+                        'zmp-prebuilt',
+                        'arm-none-eabi-gcc',
+                        subdir)
+
+
+def find_app_root(app_name):
+    '''Get absolute path of app within ZMP Zephyr SDK.'''
+    return os.path.join(find_zmp_root(), app_name)
+
+
+def find_mcuboot_root():
+    '''Get absolute path of mcuboot repository.'''
+    return os.path.join(find_zmp_root(), MCUBOOT_PATH)
+
+
+def find_mcuboot_outdir(outdir, app, board):
+    '''Get output (build) directory for MCUboot for a given app/board.'''
+    app_outdir = find_app_outdir(outdir, app, board)
+    return os.path.abspath(os.path.join(app_outdir, os.pardir, 'mcuboot'))
+
+
+def find_sdk_build_root():
+    '''Get absolute path to SDK build directory.'''
+    return os.path.dirname(os.path.realpath(__file__))
+
+
+def find_default_outdir():
+    '''Get absolute path of default output directory.'''
+    return os.path.join(find_zmp_root(), BUILD_DIR_DEFAULT)
+
+
+def find_app_outdir(outdir, app, board):
+    '''Get output (build) directory for an application.'''
+    return os.path.abspath(os.path.join(outdir, app, board, 'app'))
+
+
+def find_zephyr_board_dir(board_name):
+    '''Get directory containing Zephyr board definition, or None.'''
+    results = glob.glob(os.path.join(find_zephyr_base(),
+                                     'boards', '*', board_name))
+    if len(results) == 1:
+        return results[0]
+    return None
+
+
+def check_boards(board_names, stream=sys.stderr):
+    '''Check for Zephyr boards.
+
+    If all the boards can be found, returns without error. Otherwise,
+    prints an error and raises FileNotFoundError.'''
+    not_found = [b for b in board_names if find_zephyr_board_dir(b) is None]
+    if not_found:
+        msg = 'Unknown board{}: {}'.format('s' if len(not_found) > 1 else '',
+                                           ', '.join(not_found))
+        print(msg, file=stream)
+        raise FileNotFoundError(msg)
+
+
+def check_dependencies(programs, stream=sys.stderr):
+    '''Check for binary program dependencies.
+
+    If all the programs in the argument iterable can be found, returns
+    without error.  Otherwise, prints an error and raises
+    FileNotFoundError.'''
+    not_found = [p for p in programs if shutil.which(p) is None]
+    if not_found:
+        msg = 'Missing dependenc{}: {}'.format(
+            'ies' if len(not_found) > 1 else 'y',
+            ', '.join(not_found))
+        print(msg, file=stream)
+        raise FileNotFoundError(msg)
+
+
+def signed_app_name(app, board, app_outdir, ext):
+    app_base = os.path.basename(app)
+    app_fmt = '{}-{}-signed.{}'
+    file_name = app_fmt.format(app_base, board, ext)
+    path = os.path.join(app_outdir, 'zephyr', file_name)
+    return path
+
+#
+# Command class
+#
+
+
 # Help format strings for options shared by multiple commands.
 HELP = {
     '--board': '''Zephyr board to target (default: {}). This may be
@@ -55,14 +176,6 @@ HELP = {
     '--outputs': 'which outputs to {} (default: all)',
     'app': 'application(s) sources',
 }
-
-
-def signed_app_name(app, board, app_outdir, ext):
-    app_base = os.path.basename(app)
-    app_fmt = '{}-{}-signed.{}'
-    file_name = app_fmt.format(app_base, board, ext)
-    path = os.path.join(app_outdir, 'zephyr', file_name)
-    return path
 
 
 class Command(abc.ABC):
